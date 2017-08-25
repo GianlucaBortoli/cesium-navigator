984bccdb0b8792faa4bd01d32e9ea8b04931b19d
GianlucaBortoli <giallogiallo93@gmail.com>
2017-08-25 11:30:29 +0200

    Initial support for chrome://tracing output format #9

--- scripts/trace_splitter.py
+++ scripts/trace_splitter.py
@@ -4,21 +4,26 @@ from matplotlib import pyplot as plt
 import csv
 import argparse
 import sys
+import json
 
 
 def main(args):
     if (not args.splitCsv and not args.showChart):
         print 'No option selected. Use the -h flag to see a help message'
         sys.exit(1)
-    # select webgl function to track
+    # select webgl function to track (WTF only)
     WEBGL_FUNCTION = 'WebGLRenderingContext#'
     if (not args.function or not len(args.function)):
         WEBGL_FUNCTION += 'bindBuffer'
     else:
         WEBGL_FUNCTION += args.function
-    print 'Looking for "{}"'.format(WEBGL_FUNCTION)
+    print 'Looking for "{}"'.format(args.function)
     # get csv file path
     filePath = path.abspath(args.file)
+    # check file exixts
+    if (not path.isfile(filePath)):
+        print 'File {} not found'.format(filePath)
+        return
     if (args.splitCsv):
         split_csv(filePath, WEBGL_FUNCTION)
     if (args.showChart):
@@ -26,7 +31,48 @@ def main(args):
 
 
 def show_chart(file, functionName):
-    print 'Reading from "{}"\n'.format(file)
+    print 'Reading from {}'.format(file)
+    _, ext = path.splitext(file)
+    if (ext == '.csv'):
+        display_from_csv(file, functionName)
+    elif (ext == '.json'):
+        # remove 1st part of function name, use plain cli argument
+        display_from_json(file, functionName.split('#')[1])
+    else:
+        print 'File extension not supported'
+        return
+
+
+def show_bar_chart(xlist, ylist, functionName):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_title('"{}" elapsed times'.format(functionName))
+    ax.set_xlabel('time from application startup (ms)')
+    ax.set_ylabel('time from previous call (ms)')
+    ax.bar(xlist, ylist)
+    plt.show()
+
+
+def display_from_json(file, functionName):
+    with open(file, 'rb') as jsonfilein:
+        timestamps = []  # the X axes
+        tdurations = []  # the Y axes
+        data = json.load(jsonfilein)
+
+        for key, events in data.iteritems():
+            for e in events:
+                if type(e) is dict:
+                    name = e.get('name', '')
+                    if (name == functionName):
+                        # thread clock duration for the operation
+                        tdurations.append(e.get('tdur', 0))
+                        # tracing clock timestamp of the event
+                        timestamps.append(e.get('ts', 0))
+        assert len(timestamps) == len(tdurations)
+        show_bar_chart(timestamps, tdurations, functionName)
+
+
+def display_from_csv(file, functionName):
     with open(file, 'rb') as csvfilein:
         reader = csv.reader(csvfilein)
         next(reader)  # skip header
@@ -50,15 +96,8 @@ def show_chart(file, functionName):
                     lastTime = startTime
                     elapsedTimes.append(elapsedTime)
                     arrivalTimes.append(startTime)
-        # show bar chart
         assert len(arrivalTimes) == len(elapsedTimes)
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.set_title('"{}" elapsed times'.format(functionName.split('#')[1]))
-        ax.set_xlabel('time from application startup (ms)')
-        ax.set_ylabel('time from previous call (ms)')
-        ax.bar(arrivalTimes, elapsedTimes)
-        plt.show()
+        show_bar_chart(arrivalTimes, elapsedTimes, functionName.split('#')[1])
 
 
 def split_csv(file, functionName):
