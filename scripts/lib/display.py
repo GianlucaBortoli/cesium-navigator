10472393a1117f5886c5557210a156b35a55af9c
GianlucaBortoli <giallogiallo93@gmail.com>
2017-09-07 11:44:08 +0200

    Clean x axis on single grouped chart

--- scripts/lib/display.py
+++ scripts/lib/display.py
@@ -91,6 +91,13 @@ def display_stacked_bars_groups(file, functionName):
             ('Init', 'Modify', 'Draw', 'Other')
         )
         plt.ylabel('Cumulative time (ms)')
+        plt.tick_params(
+            axis='x',          # changes apply to the x-axis
+            which='both',      # both major and minor ticks are affected
+            bottom='off',      # ticks along the bottom edge are off
+            top='off',         # ticks along the top edge are off
+            labelbottom='off'  # labels along the bottom edge are off
+        )
         plt.show()
 
 
